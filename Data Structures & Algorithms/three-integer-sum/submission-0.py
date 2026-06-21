85b1963b6dcd4657757504a157b148009a4c1e87
Andrew Li <43484813+ANDREW-Li-33@users.noreply.github.com>
2026-06-21 13:16:03 -0400

    Add: three-integer-sum - submission-0

---
+++ Data Structures & Algorithms/three-integer-sum/submission-0.py
@@ -0,0 +1,15 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+
+
+# hashmap : {(): -1, ():  }
+
+# nums[i] + nums[j] = -num[k]
+
+# target = 
+
+[-1, 0, 1, 2, -1, -4]
+[-4, -1, -1, 0, 1, 2]
+
+#.0  1. 2. 3. 4.  5
+
